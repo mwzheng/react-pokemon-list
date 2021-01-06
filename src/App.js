76862995a7c43c0fc6624ed15f2382ceb014ad79
Mickey <mickey.zheng@yahoo.com>
2021-01-06 00:18:49 -0500

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import PokemonList from './component/PokemonList'
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('https://pokeapi.co/api/v2/pokemon/');
+  const [prevPage, setPrevPage] = useState('');
+  const [nextPage, setNextPage] = useState('');
+  const [pokeList, setPokeList] = useState([]);
+
+  useEffect(() => {
+    fetch(currentPage)
+      .then(res => res.json())
+      .then(res => {
+        setPokeList(res.results);
+        setNextPage(res.next);
+        setPrevPage(res.previous);
+      })
+  }, [currentPage]);
+
   return (
     <div className="App">
-
+      <PokemonList pokeList={pokeList} />
+      <div>
+        <a>a</a>
+      </div>
     </div>
   );
 }
